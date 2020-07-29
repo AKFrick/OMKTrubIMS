e21f2dfe65007c348edbfcf4063c4d6101369d4a
Alex <ak.frick@Outlook.com>
2020-07-29 16:19:13 +0500

    add prism entity Log.cs

---
+++ Tasker/Model/Log.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System;
+using System.Threading;
+
+namespace TaskManager.Model
+{
+    public static class Log
+    {
+        private static object syncLock = new object();
+        public static void logThis(string msg)
+        {
+            File.AppendAllText("Log.txt", $"{DateTime.Now.ToString()}:{msg}{Environment.NewLine}");
+        }
+    }
+}
