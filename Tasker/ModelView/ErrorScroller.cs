8f2e07b2d732fcd9347d611d817121976791e638
Alex <ak.frick@Outlook.com>
2020-07-30 11:32:22 +0500

    add status bar and errorscroller class

---
+++ Tasker/ModelView/ErrorScroller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasker.ModelView
+{
+    public class ErrorScroller
+    {
+        public ErrorScroller()
+        {
+            
+        }
+        public ErrorItem CurrentError { get; set; }
+
+    }
+    public class ErrorItem
+    {
+        public ErrorItem()
+        {
+
+        }
+    }
+}
