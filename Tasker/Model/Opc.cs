cb0b1b68d3d7da5e5732d4904bc2dd0cc2825200
Alex <ak.frick@Outlook.com>
2020-08-06 11:07:36 +0500

    add class opc. add settings endpoint for opc

---
+++ Tasker/Model/Opc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasker.Model
+{
+    /// <summary>
+    /// Связь с ПЛК через OPC UA
+    /// </summary>
+    public class Opc
+    {
+
+    }
+}
--- Tasker/Model/Plc.cs
+++ Tasker/Model/Plc.cs
@@ -12,7 +12,6 @@ namespace Tasker.Model
         /// <summary> Отправить задание в ПЛК </summary>
         public bool SendTask(ProductionTask task)
         {
-
             return true;
         }
     }
